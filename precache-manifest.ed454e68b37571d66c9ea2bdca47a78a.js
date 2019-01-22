2e8c647f3e8fe2d83ebcfb9b06c4e110a04d1be9
dbilenkin <dbilenkin@gmail.com>
2019-01-21 23:45:36 -0500

    Updates

---
+++ precache-manifest.ed454e68b37571d66c9ea2bdca47a78a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ae8f2aaa4c35884f9c9",
+    "url": "/static/js/main.1ae8f2aa.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "1ae8f2aaa4c35884f9c9",
+    "url": "/static/css/main.99103aa9.chunk.css"
+  },
+  {
+    "revision": "317ee54fe5449f3f5194939f03683636",
+    "url": "/index.html"
+  }
+];
